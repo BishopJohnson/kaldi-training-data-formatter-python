4d1acbc949a7c3beeb8b123b64160643a546f6c7
BishopJohnson <BishopJohnson@users.noreply.github.com>
2025-10-26 19:14:29 -0700

    Added `Chapter` class

---
+++ src/kaldi_training_data_formatter/chapter.py
@@ -0,0 +1,140 @@
+﻿class Chapter:
+
+    ### Constructors ###
+
+    def __init__(self, init_id: int):
+        self._id = init_id
+        self._project_id = 0
+        self._song_id = None
+        self._speaker_id = 0
+        self._subset = None
+
+    ### Properties ###
+
+    # Id
+    @property
+    def id(self) -> int:
+        return self._id
+
+    # ProjectId
+    @property
+    def project_id(self) -> int:
+        return self._project_id
+
+    @project_id.setter
+    def project_id(self, value: int) -> None:
+        self._project_id = value
+
+    # SongId
+    @property
+    def song_id(self) -> str:
+        return self._song_id
+
+    @song_id.setter
+    def song_id(self, value: str) -> None:
+        self._song_id = value
+
+    @song_id.deleter
+    def song_id(self) -> None:
+        del self._song_id
+
+    # SpeakerId
+    @property
+    def speaker_id(self) -> int:
+        return self._speaker_id
+
+    @speaker_id.setter
+    def speaker_id(self, value: int) -> None:
+        self._speaker_id = value
+
+    # Subset
+    @property
+    def subset(self) -> str:
+        return self._subset
+
+    @subset.setter
+    def subset(self, value: str) -> None:
+        self._subset = value
+
+    ### Functions ###
+
+    def __ge__(self, other):
+        if other is None:
+            return True
+
+        if self is other:
+            return True
+
+        if not isinstance(other, Chapter):
+            return NotImplemented
+
+        return self.id >= other.id
+
+    def __gt__(self, other):
+        if other is None:
+            return True
+
+        if self is other:
+            return False
+
+        if not isinstance(other, Chapter):
+            return NotImplemented
+
+        return self.id > other.id
+
+    def __eq__(self, other) -> bool:
+        if other is None:
+            return False
+
+        if self is other:
+            return True
+
+        if not isinstance(other, Chapter):
+            return False
+
+        return (self.id == other.id
+                and self.project_id == other.project_id
+                and ((self.song_id is None and other.song_id is None)
+                     or (self.song_id is not None and self.song_id == other.song_id))
+                and self.speaker_id == other.speaker_id
+                and ((self.subset is None and other.subset is None)
+                     or (self.subset is not None and self.subset == other.subset)))
+
+    def __hash__(self) -> int:
+        return hash(self.id)
+
+    def __le__(self, other):
+        if other is None:
+            return True
+
+        if self is other:
+            return True
+
+        if not isinstance(other, Chapter):
+            return NotImplemented
+
+        return self.id <= other.id
+
+    def __lt__(self, other):
+        if other is None:
+            return True
+
+        if self is other:
+            return False
+
+        if not isinstance(other, Chapter):
+            return NotImplemented
+
+        return self.id < other.id
+
+    def __str__(self) -> str:
+        seperator = " | "
+        str_list = [
+            str(self.id), seperator,
+            str(self.speaker_id), seperator,
+            str(self.subset), seperator,
+            str(self.project_id), seperator,
+            str(self.song_id),
+        ]
+
+        return ''.join(str_list)
---
+++ tests/unit/test_chapter.py
@@ -0,0 +1,198 @@
+﻿import unittest
+from src.kaldi_training_data_formatter import Chapter
+
+
+class TestChapter(unittest.TestCase):
+    def test_to_string_test(self):
+        param_list = [
+            # (chapter)_id, project_id, song_id, speaker_id, subset, expected
+            (1, 1, 'song', 1, 'subset', '1 | 1 | subset | 1 | song'),
+            (100, 1, 'CVRD-438', 1, 'train-clean', '100 | 1 | train-clean | 1 | CVRD-438'),
+        ]
+
+        for chapter_id, project_id, song_id, speaker_id, subset, expected in param_list:
+            with self.subTest():
+                # Arrange
+                class_under_test = Chapter(chapter_id)
+                class_under_test.project_id = project_id
+                class_under_test.song_id = song_id
+                class_under_test.speaker_id = speaker_id
+                class_under_test.subset = subset
+
+                # Act
+                actual = str(class_under_test)
+
+                # Assert
+                self.assertEqual(expected, actual)
+
+    def test_ge_when_other_is_none_returns_true(self):
+        # Arrange
+        class_under_test = Chapter(0)
+        other = None
+
+        # Act
+        actual = class_under_test >= other
+
+        # Assert
+        self.assertTrue(actual)
+
+    def test_ge_when_other_is_self_returns_true(self):
+        # Arrange
+        class_under_test = Chapter(0)
+        other = class_under_test
+
+        # Act
+        actual = class_under_test >= other
+
+        # Assert
+        self.assertTrue(actual)
+
+    def test_ge_for_given_ids_returns_expected(self):
+        param_list = [
+            # (self_)id, (other_)id, expected
+            (0, 0, True),
+            (1, 0, True),
+            (0, 1, False),
+        ]
+
+        for self_id, other_id, expected in param_list:
+            with self.subTest():
+                # Arrange
+                class_under_test = Chapter(self_id)
+                other = Chapter(other_id)
+
+                # Act
+                actual = class_under_test >= other
+
+                # Assert
+                self.assertEqual(expected, actual)
+
+    def test_gt_when_other_is_none_returns_true(self):
+        # Arrange
+        class_under_test = Chapter(0)
+        other = None
+
+        # Act
+        actual = class_under_test > other
+
+        # Assert
+        self.assertTrue(actual)
+
+    def test_gt_when_other_is_self_returns_false(self):
+        # Arrange
+        class_under_test = Chapter(0)
+        other = class_under_test
+
+        # Act
+        actual = class_under_test > other
+
+        # Assert
+        self.assertFalse(actual)
+
+    def test_gt_for_given_ids_returns_expected(self):
+        param_list = [
+            # (self_)id, (other_)id, expected
+            (0, 0, False),
+            (1, 0, True),
+            (0, 1, False),
+        ]
+
+        for self_id, other_id, expected in param_list:
+            with self.subTest():
+                # Arrange
+                class_under_test = Chapter(self_id)
+                other = Chapter(other_id)
+
+                # Act
+                actual = class_under_test > other
+
+                # Assert
+                self.assertEqual(expected, actual)
+
+    def test_le_when_other_is_none_returns_true(self):
+        # Arrange
+        class_under_test = Chapter(0)
+        other = None
+
+        # Act
+        actual = class_under_test <= other
+
+        # Assert
+        self.assertTrue(actual)
+
+    def test_le_when_other_is_self_returns_true(self):
+        # Arrange
+        class_under_test = Chapter(0)
+        other = class_under_test
+
+        # Act
+        actual = class_under_test <= other
+
+        # Assert
+        self.assertTrue(actual)
+
+    def test_le_for_given_ids_returns_expected(self):
+        param_list = [
+            # (self_)id, (other_)id, expected
+            (0, 0, True),
+            (1, 0, False),
+            (0, 1, True),
+        ]
+
+        for self_id, other_id, expected in param_list:
+            with self.subTest():
+                # Arrange
+                class_under_test = Chapter(self_id)
+                other = Chapter(other_id)
+
+                # Act
+                actual = class_under_test <= other
+
+                # Assert
+                self.assertEqual(expected, actual)
+
+    def test_lt_when_other_is_none_returns_true(self):
+        # Arrange
+        class_under_test = Chapter(0)
+        other = None
+
+        # Act
+        actual = class_under_test < other
+
+        # Assert
+        self.assertTrue(actual)
+
+    def test_lt_when_other_is_self_returns_false(self):
+        # Arrange
+        class_under_test = Chapter(0)
+        other = class_under_test
+
+        # Act
+        actual = class_under_test < other
+
+        # Assert
+        self.assertFalse(actual)
+
+    def test_lt_for_given_ids_returns_expected(self):
+        param_list = [
+            # (self_)id, (other_)id, expected
+            (0, 0, False),
+            (1, 0, False),
+            (0, 1, True),
+        ]
+
+        for self_id, other_id, expected in param_list:
+            with self.subTest():
+                # Arrange
+                class_under_test = Chapter(self_id)
+                other = Chapter(other_id)
+
+                # Act
+                actual = class_under_test < other
+
+                # Assert
+                self.assertEqual(expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
