a372b7c327acbea431a3d8e033f4bc1a0f8a595a
BishopJohnson <BishopJohnson@users.noreply.github.com>
2025-11-03 12:33:24 -0800

    Added `ProjectUtil` class

--- src/kaldi_training_data_formatter/__init__.py
+++ src/kaldi_training_data_formatter/__init__.py
@@ -1,4 +1,8 @@
-﻿from .abstract_file_reader import AbstractFileReader
+﻿# No dependencies
 from .chapter import Chapter
 from .transcript_line import TranscriptLine
+from .project_util import ProjectUtil
+
+# Depends on the above
+from .abstract_file_reader import AbstractFileReader
 from .transcript_reader import TranscriptReader
