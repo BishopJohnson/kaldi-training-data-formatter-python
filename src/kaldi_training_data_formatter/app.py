e98a612316687dd510d4d88f95d27eeb84e87b66
BishopJohnson <BishopJohnson@users.noreply.github.com>
2025-11-11 21:27:20 -0800

    Fixed wrong format method being called in `App.run()`

--- src/kaldi_training_data_formatter/app.py
+++ src/kaldi_training_data_formatter/app.py
@@ -28,7 +28,7 @@ class App:
     def run(self) -> int:
         audio_root: str = os.path.join(self.__root, 'audio')
 
-        FilesUtil.format_audio_files(audio_root)
+        FilesUtil.format_transcript_files(audio_root)
 
         self.__vocab_compiler.read_vocabulary()
         self.__vocab_compiler.save_vocabulary()
