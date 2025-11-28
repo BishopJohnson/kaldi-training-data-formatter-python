6a806a0410a308f907049c4626fe1967fbf1e683
BishopJohnson <BishopJohnson@users.noreply.github.com>
2025-11-27 20:48:03 -0800

    - Changed `App` to sort data when using sort flag - Changed `App` to compile chapters file when using compile flag

--- src/kaldi_training_data_formatter/app.py
+++ src/kaldi_training_data_formatter/app.py
@@ -2,7 +2,13 @@
 import os.path
 import traceback
 
-from kaldi_training_data_formatter import VocabCompiler, FilesUtil, LexiconCompiler
+from kaldi_training_data_formatter import \
+    VocabCompiler, \
+    FilesUtil, \
+    LexiconCompiler, \
+    SubsetSorter, \
+    ChaptersCompiler, \
+    AUDIO_DIR_NAME
 
 
 class App:
@@ -27,42 +33,74 @@ class App:
                             type=str,
                             help='The root directory to run the app in.')
         self.__args = parser.parse_args()
+        self.__root: str = self.__args.root if self.__args.root else os.getcwd()
+        self.__audio_root: str = os.path.join(self.__root, AUDIO_DIR_NAME)
+        self.__verbose: bool = self.__args.verbose
 
     def run(self) -> int:
         try:
-            root: str = self.__args.root if self.__args.root else os.getcwd()
-            audio_root: str = os.path.join(root, 'audio')
-            verbose: bool = self.__args.verbose
-
-            if self.__args.compile:  # Compile vocabulary and lexicon
+            if self.__args.compile:
                 if self.__args.format:
-                    print('Formatting transcript files')
-                    FilesUtil.format_transcript_files(audio_root, verbose=verbose)
+                    self.__format_transcript_files()
 
-                print('Compiling vocabulary and lexicon files')
-                vocab_compiler: VocabCompiler = VocabCompiler.from_root(root)
-                vocab_compiler.verbose = verbose
-                vocab_compiler.read_vocabulary()
-                vocab_compiler.save_vocabulary()
+                self.__compile_lexicon_and_vocab()
 
-                lexicon_compiler: LexiconCompiler = LexiconCompiler.from_root(root)
-                lexicon_compiler.verbose = verbose
-                lexicon_compiler.set_import_lexicons(self.__args.import_lexicons)
-                lexicon_compiler.compile_lexicon(vocab_compiler.vocabulary, use_existing=True)
-                lexicon_compiler.save_lexicon()
+                if self.__args.format:
+                    self.__format_audio_files()
 
-            FilesUtil.format_audio_files(audio_root, verbose=self.__verbose)
-            if self.__args.format:
-                print('Formatting audio files')
-                FilesUtil.format_audio_files(audio_root, verbose=verbose)
+                if self.__args.sort:
+                    self.__sort_subsets()
 
-            if self.__args.sort:
-                print('Sorting data into subsets')
+                self.__compile_chapters()
+            elif self.__args.format:
+                self.__format_transcript_files()
+                self.__format_audio_files()
+
+                if self.__args.sort:
+                    self.__sort_subsets()
+            elif self.__args.sort:
+                self.__sort_subsets()
 
             return 0
         except Exception:
             print(traceback.format_exc())
             return -1
+
+    def __compile_chapters(self) -> None:
+        print('Compiling chapter file')
+        chapters_compiler: ChaptersCompiler = ChaptersCompiler.from_root(self.__root)
+        chapters_compiler.compile_chapters()
+        chapters_compiler.write_chapters()
+
+    def __compile_lexicon_and_vocab(self) -> None:
+        print('Compiling vocabulary and lexicon files')
+        vocab_compiler: VocabCompiler = VocabCompiler.from_root(self.__root)
+        vocab_compiler.verbose = self.__verbose
+        vocab_compiler.read_vocabulary()
+        vocab_compiler.save_vocabulary()
+
+        lexicon_compiler: LexiconCompiler = LexiconCompiler.from_root(self.__root)
+        lexicon_compiler.verbose = self.__verbose
+        lexicon_compiler.set_import_lexicons(self.__args.import_lexicons)
+        lexicon_compiler.compile_lexicon(vocab_compiler.vocabulary, use_existing=True)
+        lexicon_compiler.save_lexicon()
+
+    def __format_audio_files(self) -> None:
+        print('Formatting audio files')
+        FilesUtil.format_audio_files(self.__audio_root, verbose=self.__verbose)
+
+    def __format_transcript_files(self) -> None:
+        print('Formatting transcript files')
+        FilesUtil.format_transcript_files(self.__audio_root, verbose=self.__verbose)
+
+    def __sort_subsets(self) -> None:
+        print('Sorting data into subsets')
+        sorter: SubsetSorter = (SubsetSorter.builder()
+                                .set_input_root(self.__root)
+                                .set_output_root(self.__root)
+                                .build())
+        sorter.gather_sources()
+        sorter.sort_sources()
 
 
 def cli() -> int:
